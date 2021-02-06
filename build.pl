aa78a26f398b31a398078b32835948b7ce98b94e
Ben Bullock <benkasminbullock@gmail.com>
2021-02-06 20:05:46 +0900

    Changes for CI

--- build.pl
+++ build.pl
@@ -2,8 +2,18 @@
 use warnings;
 use strict;
 use FindBin '$Bin';
+use lib "$Bin/copied/lib";
 use Perl::Build;
-perl_build (
+
+my %build = (
     make_pod => "$Bin/make-pod.pl",
 );
+
+if ($ENV{CI}) {
+    delete $build{c};
+    $build{verbose} = 1;
+    $build{no_make_examples} = 1;
+}
+
+perl_build (%build);
 exit;
