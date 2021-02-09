429d7e6f03824227c7c13f36ce8cda9025887c47
Ben Bullock <benkasminbullock@gmail.com>
2021-02-09 10:21:16 +0900

    Version; documentation
    A bit more documentation about booleans, about the JSON::Client module
    in the distribution, and about what I'm doing with the module.

--- versionup.pl
+++ versionup.pl
@@ -10,8 +10,8 @@ use FindBin '$Bin';
 use File::Slurper qw!read_text write_text!;
 use Deploy 'make_date';
 
-my $newversion = '0.00_06';
-my $version = '0.00_05';
+my $newversion = '0.00_07';
+my $version = '0.00_06';
 
 my @pmfiles = qw!
 lib/JSON/Server.pm
